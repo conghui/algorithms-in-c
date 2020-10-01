73e962a2e12b3b00c37f759ce1ba64f0ed4e1e11
pareekaabhi33 <72213935+pareekaabhi33@users.noreply.github.com>
2020-10-01 21:44:24 +0530

    Update main.c

--- chap05/26/main.c
+++ chap05/26/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-void ruler(int a, int b, int h);
-void mark(int h);
+//r represent ruler 
+void r(int a, int b, int h);
+//m represent mark
+void m(int h);
 
 int main(void)
 {
-    ruler(0, 11, 4);
-    mark(30);
-    ruler(4, 20, 4);
+    r(0, 11, 4);
+    m(30);
+    r(4, 20, 4);
     return 0;
 }
 
@@ -19,10 +21,10 @@ void ruler(int a, int b, int h)
     }
 
     mid = a + ((b - a) >> 1);
-    ruler(a, mid, h - 1);
+    r(a, mid, h - 1);
     printf("%2d|", mid);
-    mark(h);
-    ruler(mid + 1, b, h - 1);
+    m(h);
+    r(mid + 1, b, h - 1);
 }
 
 void mark(int h)
